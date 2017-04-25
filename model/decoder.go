c505e1c09fac76d91794de29f5852030ba9e6e82
Marcos Sartori <msartori@thesiriusproject.com>
2017-04-25 20:40:11 -0300

    initial commit

---
+++ model/decoder.go
@@ -0,0 +1,12 @@
+package model
+
+func (s *Model) decoderUnit(
+	validIn <-chan bool,
+	pcAddrIn <-chan uint32,
+	instructionIn <-chan []byte,
+	validOut chan<- bool,
+	instructionOut chan<- uint32) {
+
+	go func() {
+	}()
+}
