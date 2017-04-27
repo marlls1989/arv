ea06d386b371569107222ff9aedbaa0d4a17be1e
Marcos Sartori <msartori@thesiriusproject.com>
2017-04-27 13:32:28 -0300

    decoder instructions types selectors const included

--- model/decoder.go
+++ model/decoder.go
@@ -1,11 +1,32 @@
 package model
 
+type xuSelector uint8
+type opSelector uint8
+
+const (
+	xuBypassSel xuSelector = iota
+	xuAdderSel
+	xuLogicSel
+	xuShiftSel
+	xuMemorySel
+)
+
+const (
+	opRSelector opSelector = iota
+	opISelector
+	opSSelector
+	opSBSelector
+	opUSelector
+	opUJSelector
+)
+
 func (s *Model) decoderUnit(
 	validIn <-chan bool,
 	pcAddrIn <-chan uint32,
 	instructionIn <-chan []byte,
 	validOut chan<- bool,
-	instructionOut chan<- uint32) {
+	instructionOut, pcAddrOut chan<- uint32,
+	xuSel chan<- xuSelector) {
 
 	go func() {
 	}()
