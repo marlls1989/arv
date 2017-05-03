5ca00e64a26237783bce920dd2e11bc890a785b7
Marcos Sartori <msartori@thesiriusproject.com>
2017-05-03 11:17:48 -0300

    Program queue improoved

--- model/programqueue.go
+++ model/programqueue.go
@@ -1,5 +1,9 @@
 package model
 
+import (
+	"log"
+)
+
 type xuSelector uint8
 
 const (
@@ -16,26 +20,41 @@ type programAtom struct {
 	unit  xuSelector
 }
 
+func (s *Model) prgQElement(
+	fifoIn <-chan programAtom,
+	fifoOut chan<- programAtom) {
+
+	go func() {
+		defer close(fifoOut)
+
+		<-s.start
+		fifoOut <- programAtom{valid: false, unit: xuBypassSel}
+		for in := range fifoIn {
+			fifoOut <- in
+		}
+	}()
+}
+
 func (s *Model) programQueue(
 	fifoIn <-chan programAtom,
 	fifoOut chan<- programAtom,
 	depth int) {
 
-	fifo := make(chan programAtom, depth)
+	if depth < 2 {
+		log.Panic("Program ordering queue depth must be at least 2")
+	}
 
-	go func() {
-		defer close(fifo)
+	fifo := make([]chan programAtom, depth-1)
 
-		for in := range fifoIn {
-			fifo <- in
-		}
-	}()
+	for i := range fifo {
+		fifo[i] = make(chan programAtom)
+	}
 
-	go func() {
-		defer close(fifoOut)
+	s.prgQElement(fifoIn, fifo[0])
 
-		for out := range fifo {
-			fifoOut <- out
-		}
-	}()
+	for i := 1; i < depth-1; i++ {
+		s.prgQElement(fifo[i-1], fifo[i])
+	}
+
+	s.prgQElement(fifo[depth-2], fifoOut)
 }
